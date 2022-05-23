1781582e280f7c3523945077489036f9cb5c7568
GhostyO17 <oscar.castro9617@gmail.com>
2022-05-22 23:18:44 -0500

    Adds exercise 4 and 5

--- monty.h
+++ monty.h
@@ -59,5 +59,7 @@ int data;
 void pint(stack_t **stack, unsigned int line_number);
 void pop(stack_t **head, unsigned int line_number);
 void swap(stack_t **stack, unsigned int line_number);
+void add(stack_t **head, unsigned int line_number);
+void nop(stack_t **stack, unsigned int line_number);
 
 #endif
